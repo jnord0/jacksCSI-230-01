0c219e8f2c4e95839ab9966c9d1f3ee10776c7db
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2024-09-16 12:02:00 -0400

    Add files via upload

---
+++ week4/Apache-Logs.ps1
@@ -0,0 +1,28 @@
+﻿function ipVisited(){
+#couldn't get to work without doing param
+ param (
+        [string]$Page,          # The page visited or referred from (e.g., index.html, page1.html)
+        [int]$HTTPCode,         # The HTTP status code returned
+        [string]$Browser        # The name of the web browser (e.g., Chrome, Firefox)
+    )
+
+$ipRegex = [regex]'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+
+$logFilePath = "C:\xampp\apache\logs\access.log"
+$logContent = Get-Content $logFilePath
+
+ $filteredLogs = $logContent | Where-Object {
+        $_ -match $Page -and
+        $_ -match $HTTPCode -and
+        $_ -match $Browser
+    }
+
+$ipAddresses = foreach ($line in $filteredLogs) {
+        $ipRegex.Matches($line) | ForEach-Object { $_.Value }
+    }
+
+#getting the count
+$ipCounts = $ipAddresses | Group-Object | Select-Object Count, Name
+
+return $ipCounts
+}
---
+++ week4/main.ps1
@@ -0,0 +1,5 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+$results = ipVisited -Page "index.html" -HTTPCode 404 -Browser "Chrome"
+
+$results | Format-Table
