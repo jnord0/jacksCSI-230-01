5621786c54477d75e461ed2d8369e44b3ece5021
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2024-09-21 15:37:30 -0400

    Update main.ps1

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,5 +1,8 @@
-﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot Apache-Logs.ps1)
 
 $results = ipVisited -Page "index.html" -HTTPCode 404 -Browser "Chrome"
 
 $results | Format-Table
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
