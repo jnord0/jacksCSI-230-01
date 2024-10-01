f133a715de8377758afc5a37e9513bb1c828bac9
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2024-10-01 17:34:36 -0400

    Add files via upload

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,42 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+function checkPassword($password) {
+    if($password.Length -ge 6) {
+        if($password -match "[0-9]") {
+            if($password -match "[A-Z]") {
+                if($password -match "[^a-zA-Z0-9\s]") {
+                    return $true
+                }
+            }
+        }
+    }
+    Write-Host "Password does not meet requirements" | Out-String
+    return $false
+}
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
