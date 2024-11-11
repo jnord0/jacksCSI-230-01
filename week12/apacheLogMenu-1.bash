f0e14ab538b0fd5fbd1565c0fa08bd8bc247509c
Jack Nordberg <143552881+jnord0@users.noreply.github.com>
2024-11-10 20:17:05 -0500

    Add files via upload

---
+++ week12/apacheLogMenu-1.bash
@@ -0,0 +1,117 @@
+#! /bin/bash
+
+logFile="/var/log/apache2/access.log"
+ioc_file="ioc.txt"
+
+function displayAllLogs(){
+	cat "$logFile"
+}
+
+function displayOnlyIPs(){
+        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+}
+
+# function: displayOnlyPages:
+# like displayOnlyIPs - but only pages
+
+function displayOnlyPages(){
+	cat  "$logFile" | cut -d' ' -f7 | sort | uniq
+}
+
+function histogram(){
+
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+                              | uniq)
+	# This is for debugging, print here to see what it does to continue:
+	# echo "$visitsPerDay"
+
+        :> newtemp.txt  # what :> does is in slides
+	echo "$visitsPerDay" | while read -r line;
+	do
+		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+                                     | cut -d ":" -f 1)
+		local IP=$(echo "$line" | cut -d  " " -f 1)
+          
+		local newLine="$IP $withoutHours"
+		echo "$IP $withoutHours" >> newtemp.txt
+	done 
+	cat "newtemp.txt" | sort -n | uniq -c
+}
+
+# function: frequentVisitors: 
+# Only display the IPs that have more than 10 visits
+# You can either call histogram and process the results,
+# Or make a whole new function. Do not forget to separate the 
+# number and check with a condition whether it is greater than 10
+# the output should be almost identical to histogram
+# only with daily number of visits that are greater than 10 
+
+frequentVisitors() {
+	cat "$logFile" | awk '{print $1, substr($4, 2, 11)}' | sort | uniq -c | awk '$1 > 10 {print $1, $2, $3}'
+}
+
+# function: suspiciousVisitors
+# Manually make a list of indicators of attack (ioc.txt)
+# filter the records with this indicators of attack
+# only display the unique count of IP addresses.  
+# Hint: there are examples in slides
+suspiciousVisitors(){
+	grep -f "$ioc_file" "$logFile" | cut -d' ' -f1 | sort | uniq | wc -l
+}
+
+# Keep in mind that I have selected long way of doing things to 
+# demonstrate loops, functions, etc. If you can do things simpler,
+# it is welcomed.
+
+while :
+do
+	echo "PLease select an option:"
+	echo "[1] Display all Logs"
+	echo "[2] Display only IPS"
+	# Display only pages visited
+	echo "[3] Display only Pages"
+	echo "[4] Histogram"
+	# Frequent visitors
+	echo "[5] Frequent Visitors"
+	# Suspicious visitors
+	echo "[6] Suspicious Visitors"
+	echo "[7] Quit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "7" ]]; then
+		echo "Goodbye"
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		echo "Displaying all logs:"
+		displayAllLogs
+
+	elif [[ "$userInput" == "2" ]]; then
+		echo "Displaying only IPS:"
+		displayOnlyIPs
+
+	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Pages Visited:"
+		displayOnlyPages
+
+	elif [[ "$userInput" == "4" ]]; then
+		echo "Histogram:"
+		histogram
+
+        # Display frequent visitors
+	elif [[ "$userInput" == "5" ]]; then
+                echo "Frequent Visitor:"
+                frequentVisitors
+
+	elif [[ "$userInput" == "6" ]]; then
+                echo "Suspicious Visitors:"
+                suspiciousVisitors
+
+	# Display suspicious visitors
+	# Display a message, if an invalid input is given
+	fi
+done
+
